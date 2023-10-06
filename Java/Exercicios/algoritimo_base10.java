efa9f6e41ab4d4386e0b98e0cd09ed77d3ebc8de
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-06 14:33:00 -0300

    Update algoritimo_base10.java

--- Java/Exercicios/algoritimo_base10.java
+++ Java/Exercicios/algoritimo_base10.java
@@ -1,9 +1,7 @@
-import java.lang.Math;
-
 public class algoritimo_base10 {
     public static void main(String[] args) {
-        double numero = 15;
-
-        System.out.println(Math.log10(numero));
+        double numero = 15; // definindo o valor da variável como um float 15
+ 
+        System.out.println(Math.log10(numero)); // usando a biblioteca "Math" para fazer o log10
     }
 }
