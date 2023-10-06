2b828c6551ae5254b23eac02f512d2fae90ebf70
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-06 14:25:52 -0300

    Update numero_primo.java

--- Java/Exercicios/numero_primo.java
+++ Java/Exercicios/numero_primo.java
@@ -10,17 +10,17 @@ public class numero_primo {
 
     boolean primo = true;
 
-    if (numero <= 1) {
-        primo = false;
+    if (numero <= 1) { //comparando o numero com o um caso ele for menor ou igual
+        primo = false; // caso a condição acima esteja de acordo, o número não é primo
     } else {
-        for (int i = 2; i < numero; i++) {
-            if (numero % i == 0)
+        for (int i = 2; i < numero; i++) { 
+            if (numero % i == 0) // caso o resto seja igual azero, o número não é primo
             primo = false;
-            break;
+            break; // parando o loop
 }
  }
     if (primo) {
-        System.out.println("é um número primo");
+        System.out.println("é um número primo"); // printando o resultado
     } else {
         System.out.println("Não é um número primo");
     }
