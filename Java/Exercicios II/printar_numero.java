a5a3e8daff5251dd849c58962691b231f6c82b91
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-09 14:49:24 -0300

    Update printar_numero.java

--- Java/Exercicios II/printar_numero.java
+++ Java/Exercicios II/printar_numero.java
@@ -2,10 +2,10 @@ public class printar_numero {
     public static void main(String[] args) {
         Scanner Scaner = new Scanner(System.in);
 
-        System.out.print("Digite um número: ");
+        System.out.print("Digite um número: "); // pedindo um número ao usuário
         double numero = Scaner.nextDouble();
 
-        System.out.printf("Treinando pela vigésima vez como printar um número: '%.1f'", numero);
+        System.out.printf("Treinando pela vigésima vez como printar um número: '%.1f'", numero); // printando o número
     }
 }
 
