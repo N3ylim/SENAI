5d7faed88c7dca45adb5729849991c986d313ae4
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-09 14:41:50 -0300

    Update area_perimetro.java

--- Java/Exercicios II/area_perimetro.java
+++ Java/Exercicios II/area_perimetro.java
@@ -4,13 +4,13 @@ public class area_perimetro {
     public static void main(String[] args) {
          Scanner Scanner = new Scanner(System.in);
 
-        System.out.print("Escreva o valor do raio, bara obter a área e o perimetro: ");
+        System.out.print("Escreva o valor do raio, bara obter a área e o perimetro: "); // pedindo o raio ao usuário
         double raio = Scanner.nextDouble();
 
 
-        double area = 3.14 * 2 * raio;
+        double area = 3.14 * 2 * raio; // fazendo o calculo da area
 
-        System.out.println("A circunferencia do circulo: " + area);
+        System.out.println("A circunferencia do circulo: " + area); // printando o resultado
 
 
     }
