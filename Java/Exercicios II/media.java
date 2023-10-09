b13436a5a2ffcf20dfae72f2d6a600d18faeba69
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-09 14:35:55 -0300

    Update media.java

--- Java/Exercicios II/media.java
+++ Java/Exercicios II/media.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class media {
     public static void main(String[] args) {
-        int n1 = 6546;
-        int n2 = 453;
-        int n3 = 4;
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite 3 numeros para fazer a média dos números");
+        
+        System.out.print("\nN° 1: ");
+        int n1 = scanner.nextInt();
+
+        System.out.print("N° 2: ");
+        int n2 = scanner.nextInt();
+
+        System.out.print("N° 3: ");
+        int n3 = scanner.nextInt();
+        
+     
 
         int soma = n1 + n2 + n3;
         int media = soma / 3;
