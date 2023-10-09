44c35307dd4b8019221f6bbf6b86ffe3f1815406
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-09 14:43:26 -0300

    Update ola_mundo.java

--- Java/Exercicios II/ola_mundo.java
+++ Java/Exercicios II/ola_mundo.java
@@ -1,7 +1,7 @@
 public class ola_mundo {
-    public static void main(String[] args) {
-        String ola = "Olá mundo";
+    public static void main(String[] args) { 
+        String ola = "Olá mundo"; // defindo a variavel
 
-        System.out.printf("Treinando pela vigésima vez como printar um: '%s'", ola);
+        System.out.printf("Treinando pela vigésima vez como printar um: '%s'", ola); // printando o classico "hello world"
     }
 }
