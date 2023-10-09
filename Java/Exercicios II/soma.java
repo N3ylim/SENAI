3886f387267f53f9337fcf002aa87114dfe8c4a4
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-09 14:49:59 -0300

    Update soma.java

--- Java/Exercicios II/soma.java
+++ Java/Exercicios II/soma.java
@@ -4,14 +4,14 @@ public class soma {
     public static void main(String[] args) {
         Scanner Scaner = new Scanner(System.in);
 
-        System.out.print("Digite um número: ");
+        System.out.print("Digite um número: "); // pedindo dois número ao usuário
         int n1 = Scaner.nextInt();
 
         System.out.print("Digite outro número: ");
         int n2 = Scaner.nextInt();
 
-        int soma = n1 + n2;
+        int soma = n1 + n2; // fazendo o cálculo dos números pedidos
 
-        System.out.println("A soma dos numeros é: " + soma);
+        System.out.println("A soma dos numeros é: " + soma); // printando o resultado
     }
 }
