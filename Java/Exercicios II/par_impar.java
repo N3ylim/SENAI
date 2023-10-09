9b17907b83627d7f7cc2312a7486fb642071955a
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-09 14:45:38 -0300

    Update par_impar.java

--- Java/Exercicios II/par_impar.java
+++ Java/Exercicios II/par_impar.java
@@ -4,11 +4,11 @@ public class par_impar {
     public static void main(String[] args) {
          Scanner Scaner = new Scanner(System.in);
 
-        System.out.print("Digite um número: ");
+        System.out.print("Digite um número: "); // pedindo um número ao usuário
         int n1 = Scaner.nextInt();
 
-        if (n1 % 2 == 0){
-            System.out.println("O número digitado é par");
+        if (n1 % 2 == 0){ // definindo a classe do número pedido e printando o resultado
+            System.out.println("O número digitado é par"); 
         } else {
             System.out.println("O número digitado é impar");
         }
