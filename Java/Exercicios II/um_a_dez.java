dd709673e22d40ae627e5c27150b3d053f370b0e
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-09 14:50:52 -0300

    Update um_a_dez.java

--- Java/Exercicios II/um_a_dez.java
+++ Java/Exercicios II/um_a_dez.java
@@ -1,7 +1,7 @@
 public class um_a_dez {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 10; i++) { // fazendo o while para printar de 1 ao 10
             System.out.printf(i + " ");
         }
    }
