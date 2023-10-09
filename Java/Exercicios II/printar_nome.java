1fe175d9e42f070683a33e2d345ae509d4109ec8
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-09 14:48:59 -0300

    Update printar_nome.java

--- Java/Exercicios II/printar_nome.java
+++ Java/Exercicios II/printar_nome.java
@@ -2,10 +2,10 @@ public class printar_nome {
     public static void main(String[] args) {
         Scanner Scaner = new Scanner(System.in);
 
-        System.out.print("Digite seu nome: ");
+        System.out.print("Digite seu nome: "); // pedindo o nome do/ao usuário
         String nome = Scaner.nextLine();
 
-        System.out.printf("Treinando pela vigésima vez como printar o meu nome: '%s'", nome);
+        System.out.printf("Treinando pela vigésima vez como printar o meu nome: '%s'", nome); // printando o resultado
     }
 }
 
