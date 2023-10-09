0e14b32aa570baa4a78336956ffffb18d21f600f
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-09 14:38:21 -0300

    Update print_idade.java

--- Java/Exercicios II/print_idade.java
+++ Java/Exercicios II/print_idade.java
@@ -1,7 +1,10 @@
 public class print_idade {
     public static void main(String[] args) {
-        int idade = 18;
+        Scanner Scaner = new Scanner(System.in);
 
-        System.out.printf("Minha idade é: %d anos", idade);
+        System.out.print("Digite sua idade: ");
+        int idade = Scaner.nextInt();
+        
+        System.out.printf("Sua idade é: %d anos", idade);
     }
 }
