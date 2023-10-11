29191fa7b7dcceefc9d5d802e42cd075ec5b346d
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-11 16:08:57 -0300

    Delete Java/Exercicios directory

--- Java/Exercicios/print10.java
+++
@@ -1,7 +0,0 @@
-public class print10 {
-    public static void main(String[] args) {
-        int numero = 10; // Definindo o número
-
-        System.out.println(numero); //Printando o numero da varivel
-    }
-}
--- Java/Exercicios/raiz_quadrada.java
+++
@@ -1,7 +0,0 @@
-public class raiz_quadrada {
-    public static void main(String[] args) {
-
-        int numero =  81; //declarando a variavel
-        System.out.println(Math.sqrt(numero)); // usando a função "Math" para pegar a raiz do numero 
-    }
-    }
