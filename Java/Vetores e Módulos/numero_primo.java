1ad9a92563d2577f35bcc0cc2c2d6941f7f40fd9
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-18 14:33:18 -0300

    Update numero_primo.java

--- Java/Vetores e Módulos/numero_primo.java
+++ Java/Vetores e Módulos/numero_primo.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class numero_primo {
 
 
-    public static boolean ehPrimo(int numero) {
+    public static boolean ehPrimo(int numero) { // executando a função
         if (numero <= 1) {
             return false;
         }
 
-        for (int i = 2; i * i <= numero; i++) {
+        for (int i = 2; i * i <= numero; i++) { // percorrendo o numero para verificar se é primo
             if (numero % i == 0) {
                 return false;
             }
@@ -18,13 +18,13 @@ public class numero_primo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite um número: ");
+        System.out.println("Digite um número: "); // pedindo o número ao usuário
         int numero = scanner.nextInt();
 
         boolean ehPrimo = ehPrimo(numero);
 
         if (ehPrimo) {
-            System.out.println("O número é primo.");
+            System.out.println("O número é primo."); // verificando se é ou não e printando o resultado
         } else {
             System.out.println("O número não é primo.");
         }
