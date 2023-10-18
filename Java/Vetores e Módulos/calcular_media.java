45b7a17b77f5dbf583a25c5aa9673cdad0177a8b
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-18 14:30:46 -0300

    Update calcular_media.java

--- Java/Vetores e Módulos/calcular_media.java
+++ Java/Vetores e Módulos/calcular_media.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 
 public class calcular_media {
 
-    public static double calcularMedia(double[] notas) {
+    public static double calcularMedia(double[] notas) { // executando a função
         double soma = 0;
 
-        for (double nota : notas) {
+        for (double nota : notas) { // percorrendo as notas
             soma += nota;
         }
-        return soma / notas.length;
+        return soma / notas.length; // retornando o resultado
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite as notas separadas por espaços: ");
+        System.out.println("Digite as notas separadas por espaços: "); // pedindo o valor aos usuários
         String notasStr = scan.nextLine();
 
-        double[] notas = Arrays.stream(notasStr.split(" "))
+        double[] notas = Arrays.stream(notasStr.split(" ")) // criando a função
                 .mapToDouble(Double::parseDouble)
                 .toArray();
 
         double media = calcularMedia(notas);
-        System.out.println("Média das notas: " + media);
+        System.out.println("Média das notas: " + media); // printando o resultado
 
         scan.close();
     }
