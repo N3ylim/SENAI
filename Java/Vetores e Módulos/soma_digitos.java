cfccabac4345e1d0bf497fd14e0d618fda7b60de
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-18 14:39:50 -0300

    Update soma_digitos.java

--- Java/Vetores e Módulos/soma_digitos.java
+++ Java/Vetores e Módulos/soma_digitos.java
@@ -4,22 +4,22 @@ public class soma_digitos {
 
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        System.out.println("Digite um número: ");
+        System.out.println("Digite um número: "); // pedindo um número ao usuário
         int numero = ler.nextInt();
 
-        somarAlgarismos(numero);
+        somarAlgarismos(numero); // executando a função
 
         ler.close();
     }
 
-    public static void somarAlgarismos(int num) {
+    public static void somarAlgarismos(int num) { // criando a função
         int soma = 0;
         while (num > 0) {
             soma += num % 10;
             num /= 10;
         }
 
-        System.out.println("A soma entre os algarismos é: " + soma);
+        System.out.println("A soma entre os algarismos é: " + soma); // printando o resultado
     }
 
 }
