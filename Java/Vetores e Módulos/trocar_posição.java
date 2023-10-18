edb735ff463a3a8452b467509a78f8271e9e50d1
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-18 14:41:57 -0300

    Update trocar_posição.java

--- Java/Vetores e Módulos/trocar_posição.java
+++ Java/Vetores e Módulos/trocar_posição.java
@@ -3,13 +3,13 @@ import java.util.Arrays;
 
 public class trocar_posição {
     public static void main(String[] args) {
-        int[] array = {4, 7, 2, 1};
-        trocaPosicoes(array, 0, 3);
+        int[] array = {4, 7, 2, 1}; // definindo o array
+        trocaPosicoes(array, 0, 3); // executando a função
 
-        System.out.println("Array após a troca: " + Arrays.toString(array));
+        System.out.println("Array após a troca: " + Arrays.toString(array)); // printando o resultado
     }
 
-    public static void trocaPosicoes(int[] array, int pos1, int pos2) {
+    public static void trocaPosicoes(int[] array, int pos1, int pos2) { // criando a função
         int temp = array[pos1];
         array[pos1] = array[pos2];
         array[pos2] = temp;
