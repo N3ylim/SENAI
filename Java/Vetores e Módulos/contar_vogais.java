6b402839bc3ba5db7e6993f582816ee78cc3e652
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-18 14:32:11 -0300

    Update contar_vogais.java

--- Java/Vetores e Módulos/contar_vogais.java
+++ Java/Vetores e Módulos/contar_vogais.java
@@ -3,30 +3,30 @@ import java.util.Scanner;
 
 public class contar_vogais {
 
-    public static int contarVogais(String frase) {
+    public static int contarVogais(String frase) { // executando a função
 
-        int numeroDeVogais = 0;
+        int numeroDeVogais = 0; // definindo a variável
 
         for (char caractere : frase.toCharArray()) {
 
-            if (Character.isLetter(caractere) && (caractere == 'a' || caractere == 'e' || caractere == 'i'
-                    || caractere == 'o' || caractere == 'u')) {
+            if (Character.isLetter(caractere) && (caractere == 'a' || caractere == 'e' || caractere == 'i' // verificando os caracteres
+                    || caractere == 'o' || caractere == 'u')) { 
                 numeroDeVogais++;
             }
         }
 
-        return numeroDeVogais;
+        return numeroDeVogais; // retornando a função
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite uma palavra: ");
+        System.out.println("Digite uma palavra: "); // pedindo ao usuário um palavra
         String palavra = scan.nextLine();
 
         int numeroDeVogais = contarVogais(palavra);
 
-        System.out.println("Número de vogais: " + numeroDeVogais);
+        System.out.println("Número de vogais: " + numeroDeVogais); // printando o resultado
 
         scan.close();
     }
