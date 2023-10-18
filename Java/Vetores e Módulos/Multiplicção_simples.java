0dfc51482ff24e97892a431fcced45a04e52f761
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-18 14:29:26 -0300

    Update Multiplicção_simples.java

--- Java/Vetores e Módulos/Multiplicção_simples.java
+++ Java/Vetores e Módulos/Multiplicção_simples.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
 public class Multiplicção_simples {
-    public static int multi(int n1, int n2) {
+    public static int multi(int n1, int n2) { // definindo a função
         return n1 * n2;
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite o primeiro número: ");
+        System.out.println("Digite o primeiro número: "); // pedindo um número ao usuário
         int n1 = scan.nextInt();
 
-        System.out.println("Digite o segundo número: ");
+        System.out.println("Digite o segundo número: "); // pedindo um número ao usuário
         int n2 = scan.nextInt();
 
         int resultado = multi(n1, n2);
-        System.out.println("Resultado: " + resultado);
+        System.out.println("Resultado: " + resultado); // criando a função
 
         scan.close();
     }
