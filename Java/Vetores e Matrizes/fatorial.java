1f63be9df27258e901fb513959494236dfb3e6a8
Neylor <118701685+N3ylim@users.noreply.github.com>
2023-10-19 13:48:42 -0300

    Update fatorial.java

--- Java/Vetores e Matrizes/fatorial.java
+++ Java/Vetores e Matrizes/fatorial.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class fatorial {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in); 
 
-        System.out.println("Digite o primeiro numero: ");
+        System.out.println("Digite o primeiro numero: "); // pedindo um valor ao usuário
         int num = scan.nextInt();
 
-        int fatorial = calcularFatorial(num);
-        System.out.println("Fatorial de " + num + " é " + fatorial);
+        int fatorial = calcularFatorial(num); // executando a função
+        System.out.println("Fatorial de " + num + " é " + fatorial); // printando o resultado
 
         scan.close();
     }
 
     public static int calcularFatorial(int n) {
-        if (n == 0 || n == 1) {
+        if (n == 0 || n == 1) { // criando a função
             return 1;
         }
         return n * calcularFatorial(n - 1);
